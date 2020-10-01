62dd0a9aca28add3f8432ad5ec358635b5b1f875
David <david.graf28@gmail.com>
2020-10-01 14:40:43 +0200

    first commit project to understand materialize

---
+++ js/para.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+});
+
+
+let mouse=document.querySelector('.overlay');
+let lapland=document.getElementById('lapland');
+
+
+
+/*mouse.addEventListener('mouseover', function(e) {
+    e.preventDefault();
+    console.log("Enjoy Lapland !");
+    while('mouseover'){
+        document.getElementById('lapland').style.display='none';
+    }
+    document.getElementById('lapland').style.display='';
+});*/
